1a588dd293e50f856fb3df0e5557f0ef2c818a52
Pascal Stehling <stehling.pascal@gmail.com>
2019-11-15 14:02:07 +0100

    Updating travis.yml and adding simple test Case

--- main.py
+++ main.py
@@ -2,5 +2,8 @@
 This is the main File with starts the skat App
 """
 
+def hello_world():
+    return "Hello World"
+
 if __name__ == "__main__":
-    print("Hello World")
+    print(hello_world)
---
+++ main_test.py
@@ -0,0 +1,10 @@
+import main
+import unittest
+
+class TestHelloWorld(unittest.TestCase):
+
+    def test_hello(self):
+        self.assertEqual(main.hello_world() == "Hello World", "Should be Hello World")
+
+if __name__ == "__main__":
+    unittest.main()
