c5ee220db8500c65f37cf5882b54a9168410371d
Pascal Stehling <stehling.pascal@gmail.com>
2019-11-15 14:13:19 +0100

    Updated Test and travis.yml to work

--- main_test.py
+++ main_test.py
@@ -4,7 +4,7 @@ import unittest
 class TestHelloWorld(unittest.TestCase):
 
     def test_hello(self):
-        self.assertEqual(main.hello_world() == "Hello World", "Should be Hello World")
+        self.assertEqual(main.hello_world(), "Hello World")
 
 if __name__ == "__main__":
     unittest.main()
