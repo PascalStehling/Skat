3457251f0fdc351ab5284c8e92ba55e40cef7ef6
Pascal Stehling <stehling.pascal@gmail.com>
2020-02-07 09:10:29 +0100

    Added doit as build tool

---
+++ test.py
@@ -0,0 +1,17 @@
+
+from tests import player_test, settingContainer_test, Players_test, bidding_test, card_test
+import unittest
+def tests():
+    test_classes = [player_test, settingContainer_test, Players_test, bidding_test, card_test]
+
+    suit_list = []
+    loader = unittest.TestLoader()
+    for test in test_classes:
+        suit = loader.loadTestsFromModule(test)
+        suit_list.append(suit)
+
+
+    unittest.TextTestRunner(verbosity=1).run(unittest.TestSuite(suit_list))
+
+if __name__ == "__main__":
+    tests()
